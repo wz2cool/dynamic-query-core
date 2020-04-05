c72f748ab1c16547bf77e99d0be6de7780ee1530
wz2cool <wz2cool@live.cn>
2020-04-05 10:15:08 +0800

    add code coverage

--- src/test/java/com/github/wz2cool/dynamic/core/FilterDescriptorTest.java
+++ src/test/java/com/github/wz2cool/dynamic/core/FilterDescriptorTest.java
@@ -11,8 +11,40 @@ public class FilterDescriptorTest {
 
     @Test
     public void testConstructorForDefaultValue() {
-        FilterDescriptor filterDescriptor = new FilterDescriptor();
-        Assert.assertEquals(FilterCondition.AND, filterDescriptor.getCondition());
-        Assert.assertEquals(FilterOperator.EQUAL, filterDescriptor.getOperator());
+        FilterDescriptor filter = new FilterDescriptor();
+        Assert.assertEquals(FilterCondition.AND, filter.getCondition());
+        Assert.assertEquals(FilterOperator.EQUAL, filter.getOperator());
+    }
+
+    @Test
+    public void testConstructor1() {
+        FilterDescriptor filter = new FilterDescriptor();
+        filter.setCondition(FilterCondition.OR);
+        filter.setOperator(FilterOperator.CONTAINS);
+        filter.setPropertyName("name");
+        filter.setValue("frank");
+
+        Assert.assertEquals(FilterCondition.OR, filter.getCondition());
+        Assert.assertEquals(FilterOperator.CONTAINS, filter.getOperator());
+        Assert.assertEquals("name", filter.getPropertyName());
+        Assert.assertEquals("frank", filter.getValue());
+    }
+
+    @Test
+    public void testConstructor2() {
+        FilterDescriptor filter = new FilterDescriptor("age", FilterOperator.GREATER_THAN, 20);
+        Assert.assertEquals(FilterCondition.AND, filter.getCondition());
+        Assert.assertEquals(FilterOperator.GREATER_THAN, filter.getOperator());
+        Assert.assertEquals("age", filter.getPropertyName());
+        Assert.assertEquals(20, filter.getValue());
+    }
+
+    @Test
+    public void testConstructor3() {
+        FilterDescriptor filter = new FilterDescriptor(FilterCondition.OR, "age", FilterOperator.GREATER_THAN, 20);
+        Assert.assertEquals(FilterCondition.OR, filter.getCondition());
+        Assert.assertEquals(FilterOperator.GREATER_THAN, filter.getOperator());
+        Assert.assertEquals("age", filter.getPropertyName());
+        Assert.assertEquals(20, filter.getValue());
     }
 }
