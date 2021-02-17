0b5b75c2a2a0f9a49298f7c14aa1cf0a10fb785e
wz2cool <wz2cool@live.cn>
2021-02-17 22:14:59 +0800

    add test

--- src/test/java/com/github/wz2cool/dynamic/query/core/FilterDescriptorTest.java
+++ src/test/java/com/github/wz2cool/dynamic/query/core/FilterDescriptorTest.java
@@ -15,4 +15,18 @@ public class FilterDescriptorTest {
         assertEquals(FilterCondition.AND, filterDescriptor.getCondition());
         assertEquals(FilterOperator.EQUAL, filterDescriptor.getOperator());
     }
+
+    @Test
+    public void testGetSet() {
+        FilterDescriptor filterDescriptor = new FilterDescriptor();
+        filterDescriptor.setCondition(FilterCondition.OR);
+        filterDescriptor.setOperator(FilterOperator.NOT_EQUAL);
+        filterDescriptor.setPropertyName("name");
+        filterDescriptor.setValue("jack");
+
+        assertEquals(FilterCondition.OR, filterDescriptor.getCondition());
+        assertEquals(FilterOperator.NOT_EQUAL, filterDescriptor.getOperator());
+        assertEquals("name", filterDescriptor.getPropertyName());
+        assertEquals("jack", filterDescriptor.getValue());
+    }
 }
